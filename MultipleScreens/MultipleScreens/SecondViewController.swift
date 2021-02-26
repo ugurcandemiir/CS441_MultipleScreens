3dfbfc93348d9e8508d611a494cfdaa3647ddb36
ugurcandemiir <ugurcandemiir@gmail.com>
2021-02-26 12:23:36 -0500

    Created Label and Text field in View Controller and segues

--- MultipleScreens/MultipleScreens/SecondViewController.swift
+++ MultipleScreens/MultipleScreens/SecondViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- MultipleScreens/MultipleScreens/ViewController.swift
+++ MultipleScreens/MultipleScreens/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    @IBAction func doneButton(_ sender: Any) {
+        
+        
     }
 
 
